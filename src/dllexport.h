ab6106230c31f2e13001b456a1a56af3bb4719fb
Santiago M Vilar <smvilar@gmail.com>
2011-10-28 09:23:28 -0300

    dllexport include guards...

--- src/dllexport.h
+++ src/dllexport.h
@@ -1,5 +1,5 @@
-#ifndef HE_dllexport_H
-#define HE_dllexport_H
+#ifndef HE_DLLEXPORT_H
+#define HE_DLLEXPORT_H
 //----------------------------------------------------------------------------//
 // For the DLL library.
 #ifdef BUILD_DLL
@@ -15,4 +15,4 @@
 #	define ENGINE_API
 #endif // BUILD_DLL
 //----------------------------------------------------------------------------//
-#endif // HE_dllexport_H
+#endif // HE_DLLEXPORT_H
